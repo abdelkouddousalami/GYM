61c43ebd8448a18d00d328cb7531b8ff9aaf8687
abdelkouddous <abdoalami.ru@gmail.com>
2025-01-12 14:23:36 +0100

    final push

--- calorie.js
+++ calorie.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('hamburger').addEventListener('click', function () {
+        const navLinks = document.querySelector('.nav-links');
+        navLinks.classList.toggle('active');
+    });
+
+    document.getElementById('close-btn').addEventListener('click', function () {
+        const navLinks = document.querySelector('.nav-links');
+        navLinks.classList.remove('active');
+    });
+});
+
+
 document.getElementById('calorie-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
